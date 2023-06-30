bacbfdca3716144792ed232fac2fe6d5083f93ac
Ergos <bagaeff.erik@yandex.ru>
2023-06-30 22:27:37 +0300

    BottomNavigationBar

---
+++ lib/BasikDisplay/basik_display.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:ossetia/Theme/theme.dart';
+
+class BasikDisplayWidget extends StatefulWidget {
+  const BasikDisplayWidget({super.key});
+
+  @override
+  State<BasikDisplayWidget> createState() => _BasikDisplayWidgetState();
+}
+
+class _BasikDisplayWidgetState extends State<BasikDisplayWidget> {
+  int _selectedTab = 0;
+  void onSelectedTab(int index) {
+    // ignore: unrelated_type_equality_checks
+    if (onSelectedTab == index) return;
+    setState(() {
+      _selectedTab = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedTab,
+        
+      ),
+      bottomNavigationBar: Container(
+        height: 65,
+        decoration: const BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: grayColor,
+              width: 0.4,
+            ),
+          ),
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          iconSize: 27,
+          unselectedFontSize: 14,
+          currentIndex: _selectedTab,
+          unselectedItemColor: grayColor,
+          selectedItemColor: blueColor,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home_rounded,
+              ),
+              label: 'Дом',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.map,
+              ),
+              label: 'Места',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite_border,
+              ),
+              label: 'Избранное',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+              ),
+              label: 'Профиль',
+            ),
+          ],
+          onTap: (onSelectedTab),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Theme/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+MaterialColor materialWhite = MaterialColor(0xFFFFFFFF, {
+  50: const Color(0xFFFFFFFF).withOpacity(0.1),
+  100: const Color(0xFFFFFFFF).withOpacity(0.2),
+  200: const Color(0xFFFFFFFF).withOpacity(0.3),
+  300: const Color(0xFFFFFFFF).withOpacity(0.4),
+  400: const Color(0xFFFFFFFF).withOpacity(0.5),
+  500: const Color(0xFFFFFFFF).withOpacity(0.6),
+  600: const Color(0xFFFFFFFF).withOpacity(0.7),
+  700: const Color(0xFFFFFFFF).withOpacity(0.8),
+  800: const Color(0xFFFFFFFF).withOpacity(0.9),
+  900: const Color(0xFFFFFFFF).withOpacity(1.0),
+});
+const grayColor = Color(0xFF787878);
+const blueColor = Color(0xFF2B66FF);
